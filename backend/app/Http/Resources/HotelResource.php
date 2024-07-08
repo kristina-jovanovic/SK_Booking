dffa84e870dbc2b3943c0aa5b74e574f0211b633
kristina-jovanovic <jovanovick25@gmail.com>
2024-07-08 17:02:00 +0200

    dodati kontroleri, resursi i kolekcije

---
+++ backend/app/Http/Resources/HotelResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HotelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public static $wrap = 'hotel';
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'email' => $this->resource->email,
+            'adress' => $this->resource->address,
+            'restrictions' => $this->resource->restrictions,
+            'city' => new CityResource($this->resource->city),
+        ];
+    }
+}
---
+++ backend/app/Http/Resources/ReservationResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public static $wrap = 'reservation';
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'user' => new UserResource($this->resource->user),
+            'hotel' => new HotelResource($this->resource->hotel),
+            'date' => $this->resource->date,
+            'numberOfAdults' => $this->resource->numberOfAdults,
+            'numberOfChildren' => $this->resource->numberOfChildren
+        ];
+    }
+}
