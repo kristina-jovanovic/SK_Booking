81632fe08149a7bbaaf2419d3dd5e397efb3e9ce
stefanaaak <bebako01@gmail.com>
2024-07-12 13:30:27 +0200

    api za quotes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Home() {
     let navigate = useNavigate();
+    const [quote, setQuote] = useState();
+    useEffect(() => {
+        if (quote == null) {
+            axios.defaults.baseURL = "";
+
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: 'https://api.api-ninjas.com/v1/quotes?category=happiness',
+                headers: {
+                    'X-Api-Key': 'UdbC3tkRaqQWsMxfVnMNpA==rLGHxy6NzB5H8Ygg'
+                }
+            };
+
+            axios.request(config)
+                .then((response) => {
+                    // console.log(response);
+                    // console.log(response.data[0]);
+                    setQuote(response.data[0]);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            axios.defaults.baseURL = "http://127.0.0.1:8000/";
+        }
+    }, [quote]);
     return (
         // <!-- Background image -->
         <div
@@ -18,6 +45,7 @@ function Home() {
             <div className='d-flex justify-content-center align-items-center'
                 style={{ flexDirection: 'column' }}>
                 <h1 className="text-white" >Dobro došli na SK Booking!</h1>
+                {quote == null ? (<></>) : (<p className="text-white" style={{ textAlign: 'center' }}> {quote.quote} -{quote.author}</p>)}
                 <button type="button" className="btn btn-outline-info" onClick={() => (navigate('/hotels'))}>Pogledaj ponudu hotela</button>
             </div>
         </div>
